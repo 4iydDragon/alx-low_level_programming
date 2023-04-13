77923669817a68f12f4537da99813c4e08b0b395
E.k.k WT <emmanuel.k.kangogo@gmail.com>
2023-04-13 13:11:25 -0700

    0x0C. C - More malloc, free Write a function that allocates memory using malloc. Write a function that concatenates two strings. Write a function that allocates memory for an array, using malloc. Write a function that creates an array of integers. Write a function that reallocates a memory block using malloc and free Write a program that multiplies two positive numbers.

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,49 @@
+#include<stdlib.h>
+#include"main.h"
+
+/**
+* _realloc - reallocates a memory block using malloc and free
+* @ptr: pointer to the memory previsouly allocated by malloc
+* @old_size: size of the allocated memory for ptr
+* @new_size: new size of the new memory block
+* Return: pointer to the newly allocated memory block
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+char *ptr1;
+char *old_ptr;
+unsigned int j;
+
+if (new_size == old_size)
+return (ptr);
+
+if (new_size == 0 && ptr)
+{
+free(ptr);
+return (NULL);
+}
+
+if (!ptr)
+return (malloc(new_size));
+
+ptr1 = malloc(new_size);
+if (!ptr1)
+return (NULL);
+
+old_ptr = ptr;
+
+if (new_size < old_size)
+{
+for (j = 0; j < new_size; j++)
+ptr1[j] = old_ptr[j];
+}
+
+if (new_size > old_size)
+{
+for (j = 0; j < old_size; j++)
+ptr1[j] = old_ptr[j];
+}
+
+free(ptr);
+return (ptr1);
+}
