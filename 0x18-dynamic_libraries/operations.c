d80a14ac51a197d82e8a5430e73b4ff6f9262f6b
4iydDragon <emmanuel.k.kangogo@gmail.com>
2023-08-21 11:54:32 +0300

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+/**
+ *add - adds the two numbers
+ *@a: first integer number
+ *@b: second integer number
+ *Return: Always 0(Success)
+ */
+int add(int a, int b)
+{
+	int a;
+	int b;
+
+	return (a + b);
+}
+
+/**
+ *sub - subtracts two numbers
+ *@a: first integer number
+ *@b: second integer number
+ *Return: Always 0(Success)
+ */
+
+int sub(int a, int b)
+{
+	int a;
+	int b;
+
+	return (a - b);
+}
+
+/**
+ *mul - multiplication of two numbers
+ *@a: first integer number
+ *@b: second integer number
+ *Return: Always 0(Success)
+ */
+
+int mul(int a, int b)
+{
+	int a;
+	int b;
+
+	return (a * b);
+}
+
+/**
+ *division - division of two numbers
+ *@a: first integer number
+ *@b: second integer number
+ *Return: Always 0(Success)
+ */
+
+int division(int a, int b)
+{
+	int a;
+	int b;
+
+	return (a / b);
+}
+
+/**
+ *modulo - modulo of two numbers
+ *@a: first integer number
+ *@b: second integer number
+ *Return: Always 0(Success)
+ */
+
+int modulo(int a, int b)
+{
+	int a;
+	int b;
+
+	return (a % b);
+
+}
